5c7229377dee3f3fb4e4d32f876747458e4ffa20
Joseph Moussa <joeph465@gmail.com>
2021-05-19 16:28:17 -0400

    changed to healthcheck url, and added CORS middleware

--- stock_tracer/app.py
+++ stock_tracer/app.py
@@ -2,15 +2,31 @@ from fastapi import FastAPI
 from stock_tracer.database.database import engine
 from stock_tracer.database import models
 from stock_tracer.routes import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
 
-@app.get("/ping")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/healthcheck")
 async def root():
-    return {"message": "pong"}
+    return {"ping": "pong"}
 
 
 app.include_router(api_router, prefix="/api")
