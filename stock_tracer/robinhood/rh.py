d3ef7c5da82d89e24ee1c963e34160c90d5e6ef5
Joseph Moussa <joeph465@gmail.com>
2021-05-23 15:59:39 -0400

    added ability to grab robinhood data bypassing MFA with custom mfa app (will need to think about future setup)

--- stock_tracer/robinhood/rh.py
+++ stock_tracer/robinhood/rh.py
@@ -1,7 +1,10 @@
+import pyotp
 import robin_stocks.robinhood as rh
+from stock_tracer.config import config
 
 
 def fetch_build_holdings(username, password):
-    rh.login(username, password)
+    totp = pyotp.TOTP(config.robinhood.mfa_application_identifier).now()
+    rh.authentication.login(username, password, mfa_code=totp)
     my_stocks = rh.build_holdings()
     return my_stocks
