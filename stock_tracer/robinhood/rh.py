86b980e2f5a8f3cd39829e0691197ffd09159d68
Joseph Moussa <joeph465@gmail.com>
2021-05-26 18:28:04 -0400

    feature (historical_data): add ability to fetch historical data as well as get user transactions

--- stock_tracer/robinhood/rh.py
+++ stock_tracer/robinhood/rh.py
@@ -1,6 +1,7 @@
 import pyotp
 import robin_stocks.robinhood as rh
 from stock_tracer.config import config
+import yfinance as yf
 from logging import getLogger
 
 
@@ -14,4 +15,40 @@ class RobinhoodConnector:
         # totp = pyotp.TOTP(config.robinhood["mfa_application_identifier"]).now()
         # rh.authentication.login(username, password, mfa_code=totp)
         my_stocks = self.rh_conn.build_holdings()
+        self.build_holdings = my_stocks
         return my_stocks
+
+    def fetch_historicals(self, symbols: str or list = None, time_period: str = "5y"):
+        """
+        Returns a dictionary mapping ticker to a pandas dataframe interpreted dictionary of the stock's historical data
+        """
+        if symbols is None:
+            symbols = list(self.build_holdings.keys())
+        # Fetch historical data from yfinance
+        #   could also get from robinhood, but yfinance is faster and more accurate
+        response = yf.Tickers(" ".join(symbols))
+        tickers = response.tickers
+        historical_data = {}
+        for ticker, ticker_obj in tickers.items():
+            try:
+                df = ticker_obj.history(period=time_period)
+            except Exception:
+                df = ticker_obj.history(period="ytd")
+
+            ticker_historicals = df.to_dict("index")
+            historical_data[ticker] = ticker_historicals
+        return historical_data
+
+    def fetch_transactions(self):
+        stock_orders = self.rh_conn.get_all_stock_orders()
+        return stock_orders
+
+
+if __name__ == "__main__":
+    import logging
+    import json
+
+    logger = logging.getLogger(__name__)
+    rh = RobinhoodConnector(config.robinhood["username"], config.robinhood["password"])
+    transations = rh.fetch_transactions()
+    logger.warning(json.dumps(transations, indent=2))
